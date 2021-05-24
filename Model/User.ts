26d892e4a6d8bb9a8b7950047723daaa66015815
Kwame <kwameagyenimboateng64@gmail.com>
2021-05-24 23:45:03 +0000

    create user model

---
+++ Model/User.ts
@@ -0,0 +1,14 @@
+
+const data: {
+    url: String
+    email: String,
+    password: String
+    submitBtn: String
+} = {
+    url: "https://my.cowrywise.com/login",
+    email: '#email',
+    password: "[name='password']",
+    submitBtn: '.align-end'
+};
+
+export { data }
