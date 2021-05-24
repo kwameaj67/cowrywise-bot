9f95dd93fa7d01cb541e33ab1dc6188b98353604
Kwame <kwameagyenimboateng64@gmail.com>
2021-05-24 23:44:17 +0000

    server

---
+++ server.ts
@@ -0,0 +1,9 @@
+// import express = require('express');
+
+// const app = express();
+// const port = 3001
+// app.listen(port, () => {
+//     // if (err )return console.error(err)
+ 
+//     return console.log(`Server running on port ${port}`)
+// })
