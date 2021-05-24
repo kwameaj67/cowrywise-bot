f69eccb6dc45f18325106516b1dc64da59057738
Kwame <kwameagyenimboateng64@gmail.com>
2021-05-24 23:53:36 +0000

    user

---
+++ Model/User.ts
@@ -0,0 +1,13 @@
+const data: {
+    url: String
+    email: String,
+    password: String
+    submitBtn: String
+} = {
+    url: "https://my.cowrywise.com/login",
+    email: '#email',
+    password: "[name='password']",
+    submitBtn: '.align-end'
+};
+
+export { data }
